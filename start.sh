5e9ed42cdda6dbcef28b9fb0445cd6db107fae6c
yyhaoyue <derekyingyong@gmail.com>
2016-03-29 15:51:12 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./minerd -a cryptonight -o stratum+tcp://monerohash.com:3333 -u 49ZCvwXDzrM2H7iuRzkHP6XPLdFRamd4wLf5RSneSPZjcDR4Jytb2ouAthWS9kAcdoLZrXsVXGF9A4uzWiLjZsQrG93SPjH -p x -t xx
+./minerd -a cryptonight -o stratum+tcp://xmr.crypto-pool.fr:3333 -u 49ZCvwXDzrM2H7iuRzkHP6XPLdFRamd4wLf5RSneSPZjcDR4Jytb2ouAthWS9kAcdoLZrXsVXGF9A4uzWiLjZsQrG93SPjH -p x -t 21
